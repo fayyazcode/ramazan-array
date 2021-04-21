c254c7fc442a533f02d6507702c48c6efa6e51dc
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-21 07:01:45 -0700

    array calander

---
+++ array.js
@@ -0,0 +1,127 @@
+var input=+prompt("Enter a number")
+var date=[30];
+    date[0]=("<h3> Sehri </h3> 04:51 AM "+"<h3>Iftar </h3> : 6:55 PM "+"<h4>Date</h4> 14 April 2021")
+    date[1]=("<h3> Sehri </h3> 04:50 AM "+"<h3>Iftar </h3> : 6:55 PM "+"<h4>Date</h4> 15 April 2021")
+    date[2]=("<h3> Sehri </h3> 04:49 AM "+"<h3>Iftar </h3> : 6:56 PM "+"<h4>Date</h4> 16 April 2021")
+    date[3]=("<h3> Sehri </h3> 04:48 AM "+"<h3>Iftar </h3> : 6:56 PM "+"<h4>Date</h4> 17 April 2021")
+    date[4]=("<h3> Sehri </h3> 04:47 AM "+"<h3>Iftar </h3> : 6:57 PM "+"<h4>Date</h4> 18 April 2021")
+    date[5]=("<h3> Sehri </h3> 04:46 AM "+"<h3>Iftar </h3> : 6:57 PM "+"<h4>Date</h4> 19 April 2021")
+    date[6]=("<h3> Sehri </h3> 04:45 AM "+"<h3>Iftar </h3> : 6:57 PM "+"<h4>Date</h4> 20 April 2021")
+    date[7]=("<h3> Sehri </h3> 04:44 AM "+"<h3>Iftar </h3> : 6:58 PM "+"<h4>Date</h4> 21 April 2021")
+    date[8]=("<h3> Sehri </h3> 04:43 AM "+"<h3>Iftar </h3> : 6:58 PM "+"<h4>Date</h4> 22 April 2021")
+    date[9]=("<h3> Sehri </h3> 04:42 AM "+"<h3>Iftar </h3> : 6:59 PM "+"<h4>Date</h4> 23 April 2021")
+    date[10]=("<h3> Sehri </h3> 04:41 AM "+"<h3>Iftar </h3> : 6:59 PM "+"<h4>Date</h4> 24 April 2021")
+    date[11]=("<h3> Sehri </h3> 04:40 AM "+"<h3>Iftar </h3> : 7:00 PM "+"<h4>Date</h4> 25 April 2021")
+    date[12]=("<h3> Sehri </h3> 04:39 AM "+"<h3>Iftar </h3> : 7:00 PM "+"<h4>Date</h4> 26 April 2021")
+    date[13]=("<h3> Sehri </h3> 04:38 AM "+"<h3>Iftar </h3> : 7:01 PM "+"<h4>Date</h4> 27 April 2021")
+    date[14]=("<h3> Sehri </h3> 04:37 AM "+"<h3>Iftar </h3> : 7:01 PM "+"<h4>Date</h4> 28 April 2021")
+    date[15]=("<h3> Sehri </h3> 04:36 AM "+"<h3>Iftar </h3> : 7:02 PM "+"<h4>Date</h4> 29 April 2021")
+    date[16]=("<h3> Sehri </h3> 04:35 AM "+"<h3>Iftar </h3> : 7:02 PM "+"<h4>Date</h4> 30 April 2021")
+    date[17]=("<h3> Sehri </h3> 04:34 AM "+"<h3>Iftar </h3> : 7:03 PM "+"<h4>Date</h4> 01 May 2021")
+    date[18]=("<h3> Sehri </h3> 04:33 AM "+"<h3>Iftar </h3> : 7:03 PM "+"<h4>Date</h4> 02 May 2021")
+    date[19]=("<h3> Sehri </h3> 04:32 AM "+"<h3>Iftar </h3> : 7:04 PM "+"<h4>Date</h4> 03 May 2021")
+    date[20]=("<h3> Sehri </h3> 04:31 AM "+"<h3>Iftar </h3> : 7:04 PM "+"<h4>Date</h4> 04 May 2021")
+    date[21]=("<h3> Sehri </h3> 04:30 AM "+"<h3>Iftar </h3> : 7:05 PM "+"<h4>Date</h4> 05 May 2021")
+    date[22]=("<h3> Sehri </h3> 04:29 AM "+"<h3>Iftar </h3> : 7:05 PM "+"<h4>Date</h4> 06 May 2021")
+    date[23]=("<h3> Sehri </h3> 04:28 AM "+"<h3>Iftar </h3> : 7:06 PM "+"<h4>Date</h4> 07 May 2021")
+    date[24]=("<h3> Sehri </h3> 04:28 AM "+"<h3>Iftar </h3> : 7:06 PM "+"<h4>Date</h4> 08 May 2021")
+    date[25]=("<h3> Sehri </h3> 04:27 AM "+"<h3>Iftar </h3> : 7:07 PM "+"<h4>Date</h4> 09 May 2021")
+    date[26]=("<h3> Sehri </h3> 04:26 AM "+"<h3>Iftar </h3> : 7:07 PM "+"<h4>Date</h4> 10 May 2021")
+    date[27]=("<h3> Sehri </h3> 04:25 AM "+"<h3>Iftar </h3> : 7:08 PM "+"<h4>Date</h4> 11 May 2021")
+    date[28]=("<h3> Sehri </h3> 04:24 AM "+"<h3>Iftar </h3> : 7:08 PM "+"<h4>Date</h4> 12 May 2021")
+    date[29]=("<h3> Sehri </h3> 04:24 AM "+"<h3>Iftar </h3> : 7:09 PM "+"<h4>Date</h4> 13 May 2021")
+
+
+    if(input==1){
+        document.write(date[0])
+    }
+    else if(input==2){
+        document.write(date[1])
+    }
+    else if(input==3){
+        document.write(date[2])
+    }
+    else if(input==4){
+        document.write(date[3])
+    }
+    else if(input==5){
+        document.write(date[4])
+    }
+    else if(input==6){
+        document.write(date[5])
+    }
+    else if(input==7){
+        document.write(date[6])
+    }
+    else if(input==8){
+        document.write(date[7])
+    }
+    else if(input==9){
+        document.write(date[8])
+    }
+    else if(input==10){
+        document.write(date[9])
+    }
+    else if(input==11){
+        document.write(date[10])
+    }
+    else if(input==12){
+        document.write(date[11])
+    }
+    else if(input==13){
+        document.write(date[12])
+    }
+    else if(input==14){
+        document.write(date[13])
+    }
+    else if(input==15){
+        document.write(date[14])
+    }
+    else if(input==16){
+        document.write(date[15])
+    }
+    else if(input==17){
+        document.write(date[16])
+    }
+    else if(input==18){
+        document.write(date[17])
+    }
+    else if(input==19){
+        document.write(date[18])
+    }
+    else if(input==20){
+        document.write(date[19])
+    }
+    else if(input==21){
+        document.write(date[20])
+    }
+    else if(input==22){
+        document.write(date[21])
+    }
+    else if(input==23){
+        document.write(date[22])
+    }
+    else if(input==24){
+        document.write(date[23])
+    }
+    else if(input==25){
+        document.write(date[24])
+    }
+    else if(input==26){
+        document.write(date[25])
+    }
+    else if(input==27){
+        document.write(date[26])
+    }
+    else if(input==28){
+        document.write(date[27])
+    }
+    else if(input==29){
+        document.write(date[28])
+    }
+    else if(input==30){
+        document.write(date[29])
+    }
+    else{
+        alert("Invalid Number")
+    }
